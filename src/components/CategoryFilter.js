6726079a16f62a357460022475d0aa0bd3bd1d43
Kevin <magavakevin10@gmail.com>
2022-08-09 15:03:20 -0400

    commit tree ecbb66069d19393f797acf8578e109b3ef4a5794

--- src/components/CategoryFilter.js
+++ src/components/CategoryFilter.js
@@ -1,10 +1,24 @@
 import React from "react";
 
-function CategoryFilter() {
+function CategoryFilter({ categories, filter, setFilter }) {
+
+  const createButtons = categories.map((e) => {
+    const isSelected = e === filter ? 'selected' : null
+    return (
+      <button
+        key={e}
+        className={isSelected}
+        onClick={() => setFilter(e)}
+      >{e}</button>
+    )
+  }
+  )
+
   return (
     <div className="categories">
       <h5>Category filters</h5>
       {/* render <button> elements for each category here */}
+      {createButtons}
     </div>
   );
 }
--- src/components/NewTaskForm.js
+++ src/components/NewTaskForm.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 
-function NewTaskForm() {
+function NewTaskForm({ categories, onTaskFormSubmit }) {
+  const [text, setText] = useState("")
+  const [selectedCat, setSelectedCat] = useState("Code")
+
+  const makeOption = categories.filter((e)=> e !== "All")
+  .map((e, i) => {return (<option key={i} >{e}</option>)})
+  // console.log(makeOption)
+  
+
+  function handleSubmit (e){
+    e.preventDefault()
+    onTaskFormSubmit({text:text, category:selectedCat})
+  }
+
+  function handleNewText(e){
+    setText(e.target.value)
+  }
+
+  function handleNewCategory(e){
+    setSelectedCat(e.target.value)
+  }
+
+
   return (
-    <form className="new-task-form">
+    //On submit we prevent default. Then we need to send information upwards (callback)
+    //The information should be an object containing
+    <form className="new-task-form" onSubmit={handleSubmit}>
       <label>
         Details
-        <input type="text" name="text" />
+        <input type="text" name="text" onChange={handleNewText}/>
       </label>
       <label>
         Category
-        <select name="category">
+        <select name="category" onChange={handleNewCategory}>
           {/* render <option> elements for each category here */}
+          {makeOption}
         </select>
       </label>
       <input type="submit" value="Add task" />
